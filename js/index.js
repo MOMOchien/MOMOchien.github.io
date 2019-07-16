1125038abd1c0de941315b238c1f8238b7d6801a
MOMO <zxcvbnm0006326@gmail.com>
2019-07-16 12:39:06 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var NavBarChange = () => {
 	var burger = document.querySelector('.burger');
 	var nav_list = document.querySelector('.nav_list');
@@ -9,16 +8,3 @@ var NavBarChange = () => {
 }
 
 NavBarChange();
-=======
-const navBar = () =>{
-	const nav_rwd = document.querySelector('.nav_rwd');
-	const nav = document.querySelector('.ul_class');
-
-	nav_rwd.addEventListener('click', () => {
-		console.log(nav);
-		nav.classList.toggle('ul-active');
-	});
-}
-
-navBar();
->>>>>>> b8c6016c45d23d0dfb710a1c9645f1a9bde084f5
